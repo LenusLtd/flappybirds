aaed2aa30811665c5b7fed4914dfb752b7b7cc62
LenusLtd <Jordon@acusu.com>
2017-04-27 11:41:55 +0100

    More updates

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var mainStage = {
       
         //Load the bird Sprite
         game.load.image('Bird','assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
     create: function() {
         // THis Function is called after the preload function
@@ -31,6 +32,12 @@ var mainStage = {
         var spaceBar = game.input.keyboard.addKey(
                     Phaser.Keyboard.SPACEBAR);
         spaceBar.onDown.add(this.jump,this);
+        
+        //Create an empty group
+        this.pipes = game.add.group();
+        
+        //Timer for pipes
+        this.timer =game.time.events.loop(1500, this.addRowOfPipes, this);
     },
     update: function () {
         // This function s called 60 times per second
@@ -49,7 +56,37 @@ var mainStage = {
     restartGame: function(){
     //Start the 'Main' State, which restarts the game
     game.state.start('main');
+        
+        //Add a pipe
+        addOnePipe: function(x, y){
+            //Create a pipe at the position x and y
+            var pipe = game.add.sprite(x, y, 'pipe');
+            
+            //Add pipe to group 
+            this.pipes.add.(pipe);
+            
+            //Enable the pysics on the pipe
+            game.physics.arcade.enable(pipe);
+            
+            //add Velocity to pipe to make it move left
+            pipe.body.velocity.x = -200;
+            
+            //Automatically kill pipe when it is no longer visable 
+            pipe.checkWorldBounds = true;
+            pipe.outOfBoundsKill = true;
+        },
 },
+    //many pipes
+    addRowOfPipes: function(){
+        // randomly pick a numver between 1 and 5 
+        //This will be the hole position in the pipe
+        var hole = Math.floor(Math.random() *5) + 1;
+        
+        //add 6 pipes
+        for (var i =0; i <8; i++)
+            if (i !=hole && i!= hole +1)
+                this.addOnePipe(400, I *60 + 10);
+    };
 };
 
 //initialise phaser, and create a 400px x 490px game 
